7d7fd2f729ff848cb6ed30437498685b7baefac3
OskarKozaczka <56255713+OskarKozaczka@users.noreply.github.com>
2022-03-30 16:10:44 +0200

    .

--- js/main.js
+++ js/main.js
@@ -7,6 +7,28 @@ window.onload = () => {
   }
 }
 
+
+function parseURLParams(url) {
+  var queryStart = url.indexOf("?") + 1,
+      queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+      query = url.slice(queryStart, queryEnd - 1),
+      pairs = query.replace(/\+/g, " ").split("&"),
+      parms = {}, i, n, v, nv;
+
+  if (query === url || query === "") return;
+
+  for (i = 0; i < pairs.length; i++) {
+      nv = pairs[i].split("=", 2);
+      n = decodeURIComponent(nv[0]);
+      v = decodeURIComponent(nv[1]);
+
+      if (!parms.hasOwnProperty(n)) parms[n] = [];
+      parms[n].push(nv.length === 2 ? v : null);
+  }
+  return parms;
+}
+
+
 function geoFindMe() {
 
   const status = document.querySelector('#status');
@@ -23,7 +45,25 @@ function geoFindMe() {
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
     mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
     //window.location.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    window.location.href = `https://www.mbruzda.github.io/map.html?a=${latitude}&b=${longitude}}`;
+   // window.location.href = `https://www.mbruzda.github.io/map.html?a=${latitude}&b=${longitude}`;
+
+
+        // Creating map options
+    var mapOptions = {
+      center: [17.385044, 78.486671],
+      zoom: 10
+    }
+
+    // Creating a map object
+    var map = new L.map('map', mapOptions);
+
+    // Creating a Layer object
+    var params = parseURLParams(window.location.href)
+
+    var layer = new L.TileLayer(`http://{s}.tile.openstreetmap.org/${latitude}/${longitude}/10.png`);
+
+    // Adding layer to the map
+    map.addLayer(layer);
   }
 
   function error() {
